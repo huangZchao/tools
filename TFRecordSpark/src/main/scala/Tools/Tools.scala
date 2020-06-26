ddfcf2c11c4d1d06d4f9298aa90aad82036654c2
huangZchao <604217824@qq.com>
2020-06-26 23:17:58 +0800

    update

---
+++ TFRecordSpark/src/main/scala/Tools/Tools.scala
@@ -0,0 +1,26 @@
+package Tools
+
+import java.io.File
+
+object Tools {
+
+  //删除目录和文件
+  def dirDel(path: File) {
+    if (!path.exists())
+      return
+    else if (path.isFile) {
+      path.delete()
+      println(path + ":  文件被删除")
+      return
+    }
+
+    val file: Array[File] = path.listFiles()
+    for (d <- file) {
+      dirDel(d)
+    }
+
+    path.delete()
+    println(path + ":  目录被删除")
+
+  }
+}
